f9c54dfbb10e240b6ea6bdf4d19b239d56d2fc51
Pavel Krupko <pavel.krupko@qa-power.com>
2011-12-26 12:43:56 +0200

    slide 5 in ppt

---
+++ slides/1.0/js/slide.js
@@ -0,0 +1,5 @@
+var slide_1_0 = document.getElementById('slide-1_0');
+slide_1_0.addEventListener('click',function(e){
+    var zaldiarBox = document.querySelector('#slide-1_0 .zaldiar');
+    zaldiarBox.addClass('active');
+},false);

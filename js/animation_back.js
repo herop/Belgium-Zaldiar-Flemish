e2dbb3528b51dc46d4a0dcc827b3ef0a2881dc5c
Sergey Tkalych <sergey.tkalych@qa-power.com>
2011-12-27 17:03:45 +0200

    added animation for slide 5.0

--- js/animation_back.js
+++ js/animation_back.js
@@ -2,4 +2,17 @@ animation.create('#betereGiTolerantieDanTramadol_Back bar',{
 	properties:{height:'attr'},
 	delay:[0, 200, 200, 200, 200, 200, 200, 200],
 	duration:400
-});;
+});
+animation.create('#slide50 .graph .orange-line', {
+	properties:{opacity:1},
+	delay:['click'],
+	duration:1000,
+	dispatch: 'eventSlide50FirstClick'
+});
+animation.create('#slide50 .graph .gray-lines, #slide50 .graph .yellow-line, #slide50 .graph ul', {
+	properties:{opacity:1},
+	delay:['click', 1000, 1000],
+	queue:[1, 0, 2],
+	duration:1000,
+	waitfor: 'eventSlide50FirstClick'
+});
